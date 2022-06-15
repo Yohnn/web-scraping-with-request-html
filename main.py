0c1112e5e48e075bd4015e3b97df4df2856f4d27
Leon Sergio Dela Cruz <leonsergio.delacruz@gmail.com>
2022-06-15 16:04:09 +0800

    Practiced parsing articles in simple html

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ with open('simple.html') as html_file:
     source = html_file.read()
     html = HTML(html = source)
 
-print(html.html)
+articles = html.find('div.article')
+
+for article in articles:
+    headline = article.find('h2', first = True).text
+    summary = article.find('p', first = True).text
+    print(headline)
+    print(summary)
